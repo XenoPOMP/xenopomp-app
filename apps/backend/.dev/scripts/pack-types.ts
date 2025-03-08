2d23a566ef813f637958f0a83cec19bd581dcb6b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-08 22:28:44 +0200

    📜 create pack script

---
+++ apps/backend/.dev/scripts/pack-types.ts
@@ -0,0 +1,28 @@
+import { writeFile } from 'node:fs/promises';
+// @ts-expect-error Just a script...
+import path from 'node:path';
+import { cwd } from 'node:process';
+import { PackageJson } from 'type-fest';
+
+// Constants
+const LIB_NAME = '@repo/backend-types';
+const PATH_TO_OUTPUT = '../../packages/prisma-model';
+
+(async () => {
+  /** Source directory for library. */
+  const libSrcDir = path.join(cwd(), PATH_TO_OUTPUT);
+
+  /** Package.json from library folder. */
+  const packageJson: PackageJson = require(
+    path.join(libSrcDir, 'package.json'),
+  );
+
+  /** Setup package data. */
+  packageJson.name = LIB_NAME;
+
+  // Update file
+  await writeFile(
+    path.join(libSrcDir, 'package.json'),
+    JSON.stringify(packageJson, null, 2),
+  );
+})();
