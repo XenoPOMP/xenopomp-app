549ee22ecfcc474c0b9c8df6a72909121b04f32f
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-08 22:57:23 +0200

    🗂️ organize package

--- packages/app-constants/index.ts
+++ packages/app-constants/index.ts
@@ -1,4 +1,1 @@
-/** All constant ports for all app. */
-export const PORTS = {
-  BACKEND: 3001,
-};
+export * from './src/ports';
---
+++ packages/app-constants/src/ports.ts
@@ -0,0 +1,4 @@
+/** All constant ports for all app. */
+export const PORTS = {
+  BACKEND: 3001,
+};
