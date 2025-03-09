1f5ca7f149b37894f25185d0c26a9aea34d568f7
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-09 16:50:12 +0200

    📢 add template

--- turbo/generators/actions/package.ts
+++ turbo/generators/actions/package.ts
@@ -3,7 +3,7 @@ import { PlopTypes } from '@turbo/gen';
 export const packageActions: PlopTypes.ActionType[] = [
   {
     type: 'add',
-    path: '{{ turbo.paths.root }}/{{ dashCase packageName }}',
-    templateFile: 'templates/turborepo-generators.hbs',
+    path: '{{ turbo.paths.root }}/{{ dashCase packageName }}/package.json',
+    templateFile: 'templates/package/packageJson.hbs',
   },
 ];
