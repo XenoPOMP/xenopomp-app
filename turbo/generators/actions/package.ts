22c95085c57350a9af6f6f14043781eff4203b92
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-09 16:56:46 +0200

    🗄️ add templates

--- turbo/generators/actions/package.ts
+++ turbo/generators/actions/package.ts
@@ -12,4 +12,6 @@ const createTemplate = (
 export const packageActions: PlopTypes.ActionType[] = [
   createTemplate('package.json', 'packageJson.hbs'),
   createTemplate('tsconfig.json', 'tsconfig.hbs'),
+  createTemplate('eslint.config.ts', 'eslint.hbs'),
+  createTemplate('index.ts', 'index.hbs'),
 ];
