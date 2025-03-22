bd403968f7a862f95b1aaca4c36dae728293689a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-23 00:28:44 +0200

    🍪 setup cookie parser

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 
 import { PORTS } from '@repo/constants';
 
@@ -6,6 +7,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [process.env.FRONT_END_HOST],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
+
   await app.listen(PORTS.BACKEND);
 }
 bootstrap();
