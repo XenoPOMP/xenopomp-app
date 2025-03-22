4cea229c8911c4b0f8509f23c3373fa48cd623f9
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-22 17:24:47 +0200

    🖊️ fix exports

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './features';
-import { AuthModule } from './routes/auth/auth.module';
+import { AuthModule } from './routes/auth';
 import { ProjectsModule } from './routes/projects';
 import { UserModule } from './routes/user';
 
