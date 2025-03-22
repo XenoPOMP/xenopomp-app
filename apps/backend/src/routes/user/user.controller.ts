254ddae0acc507013cab536ef116c646b3126fda
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-22 19:53:33 +0200

    🚮 delete test endpoint

--- apps/backend/src/routes/user/user.controller.ts
+++ apps/backend/src/routes/user/user.controller.ts
@@ -1,18 +1,9 @@
-import { Body, Controller } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
-import { Endpoint } from '../../decorators';
-
-// eslint-disable-next-line ts/consistent-type-imports
-import { UserDto } from './dto';
 // eslint-disable-next-line ts/consistent-type-imports
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Endpoint('POST', '/test', {
-    validate: true,
-  })
-  async test(@Body() dto: UserDto) {}
 }
