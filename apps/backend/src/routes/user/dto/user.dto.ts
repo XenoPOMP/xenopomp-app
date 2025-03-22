5866088c2cba164c30bf7ccd1105ec4c92f95265
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-22 19:37:14 +0200

    ✍️ setup validation errors

--- apps/backend/src/routes/user/dto/user.dto.ts
+++ apps/backend/src/routes/user/dto/user.dto.ts
@@ -1,14 +1,19 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
-import { ERROR_CODES } from '@repo/errors';
+import { issueErrorCode } from '@repo/errors';
 
 export class UserDto {
-  @IsEmail()
+  @IsEmail(
+    {},
+    {
+      message: issueErrorCode('VALIDATION_FAILED_IS_EMAIL'),
+    },
+  )
   email: string;
 
   @IsString()
   @MinLength(6, {
-    message: `ERR${ERROR_CODES.VALIDATION_FAILED_ON_PASSWORD_MIN_LENGTH}`,
+    message: issueErrorCode('VALIDATION_FAILED_ON_PASSWORD_MIN_LENGTH'),
   })
   password: string;
 }
--- packages/errors/src/error-codes.ts
+++ packages/errors/src/error-codes.ts
@@ -7,4 +7,9 @@
 export enum ERROR_CODES {
   VALIDATION_FAILED = 100,
   VALIDATION_FAILED_ON_PASSWORD_MIN_LENGTH,
+  VALIDATION_FAILED_IS_EMAIL,
+}
+
+export function issueErrorCode(code: keyof typeof ERROR_CODES) {
+  return `ERR${code}`;
 }
