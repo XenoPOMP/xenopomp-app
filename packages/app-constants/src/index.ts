b16723fe278d5be5a907669f6fc48fc80d8d9286
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-23 18:07:19 +0200

    📝 add new constants

--- packages/app-constants/src/index.ts
+++ packages/app-constants/src/index.ts
@@ -1,3 +1,4 @@
 export * from './ports';
 export * from './access-tokens';
 export * from './perms';
+export * from './strings';
---
+++ packages/app-constants/src/strings.ts
@@ -0,0 +1,6 @@
+export const STRING_NAMES = {
+  ABOUT_ME: {
+    TITLE: 'ABOUT_ME_TITLE',
+    DESC: 'ABOUT_ME_DESC',
+  },
+} as const;
