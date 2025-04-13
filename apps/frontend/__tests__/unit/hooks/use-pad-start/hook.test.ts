720feb769bf977ab56f5b3d933826a169c07b99a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-04-13 19:42:34 +0200

    🧪 fix some tests

--- apps/frontend/__tests__/unit/hooks/use-pad-start/hook.test.ts
+++ apps/frontend/__tests__/unit/hooks/use-pad-start/hook.test.ts
@@ -1,17 +1,42 @@
-import { cleanup } from '@testing-library/react';
+import { cleanup, renderHook } from '@testing-library/react';
 import { afterEach, describe, expect, test } from 'vitest';
 
 import { usePadStart } from '@/hooks';
 
-import { assertHookRendering, createUsePadStartTest } from '@test/assets';
+import {
+  assertHookRendering,
+  createUsePadStartTest,
+  injectMatchMediaMock,
+} from '@test/assets';
 
 describe('usePadStart', () => {
+  injectMatchMediaMock();
+
   afterEach(() => {
     cleanup();
   });
 
   test('It renders', () => {
-    assertHookRendering(() => usePadStart());
+    assertHookRendering(() =>
+      usePadStart('12', {
+        maxLength: 3,
+        fillString: '0',
+      }),
+    );
+  });
+
+  test('Fill string is applied', () => {
+    const {
+      result: { current },
+    } = renderHook(() =>
+      usePadStart('12', {
+        maxLength: 3,
+        fillString: '0',
+      }),
+    );
+
+    expect(current?.pad).toBe('0');
+    expect(current.input).toBe('12');
   });
 
   test('Testing with test component', () => {
