cac7fbcb34f7cb29a0ddf955a2e66eb576558c5a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-04-13 19:29:09 +0200

    💉 change font mocks

--- apps/frontend/__tests__/unit/app/layout.test.tsx
+++ apps/frontend/__tests__/unit/app/layout.test.tsx
@@ -10,12 +10,38 @@ describe('Root layout test', () => {
   injectMatchMediaMock();
 
   injectMocks(() => {
-    vi.mock('next/font/google', () => ({
-      Inter: FONT_MOCK,
-      Playwrite_DE_LA: FONT_MOCK,
-      Sofia_Sans: FONT_MOCK,
-    }));
+    vi.mock(import('next/font/google'), async importOriginal => {
+      const mod = await importOriginal();
+      return {
+        ...mod,
+        // eslint-disable-next-line jsdoc/require-jsdoc
+        Sofia_Sans: () => ({
+          className: '<MOCKED>',
+          style: {
+            fontFamily: '<MOCKED_FONT_FAMILY>',
+          },
+          variable: '<MOCKED_VAR_NAME>',
+        }),
+
+        // eslint-disable-next-line jsdoc/require-jsdoc
+        Playwrite_DE_LA: () => ({
+          className: '<MOCKED>',
+          style: {
+            fontFamily: '<MOCKED_FONT_FAMILY>',
+          },
+          variable: '<MOCKED_VAR_NAME>',
+        }),
+      };
+    });
   });
+
+  // injectMocks(() => {
+  //   vi.mock('next/font/google', () => ({
+  //     Inter: FONT_MOCK,
+  //     Playwrite_DE_LA: FONT_MOCK,
+  //     Sofia_Sans: FONT_MOCK,
+  //   }));
+  // });
 
   testNextPage(<RootLayout children={undefined} />, {
     generateMetadata,
--- apps/frontend/__tests__/unit/components/ui-kit/logo/component.test.tsx
+++ apps/frontend/__tests__/unit/components/ui-kit/logo/component.test.tsx
@@ -1,10 +1,27 @@
-import { describe, test } from 'vitest';
+import { describe, test, vi } from 'vitest';
 
 import { Logo } from '@/components/ui/kit';
 
-import { assertRendering } from '@test/assets';
+import { assertRendering, injectMocks } from '@test/assets';
 
 describe('Logo component', () => {
+  injectMocks(() => {
+    vi.mock(import('next/font/google'), async importOriginal => {
+      const mod = await importOriginal();
+      return {
+        ...mod,
+        // eslint-disable-next-line jsdoc/require-jsdoc
+        Playwrite_DE_LA: () => ({
+          className: '<MOCKED>',
+          style: {
+            fontFamily: '<MOCKED_FONT_FAMILY>',
+          },
+          variable: '<MOCKED_VAR_NAME>',
+        }),
+      };
+    });
+  });
+
   test('It renders', () => {
     assertRendering(<Logo />);
   });
