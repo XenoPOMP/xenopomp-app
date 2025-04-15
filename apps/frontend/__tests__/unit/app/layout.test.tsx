7ae0fb8374a2ff5fa29bb11932ae8c0bf7295af6
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-04-15 16:03:21 +0200

    🧪 change mocking

--- apps/frontend/__tests__/unit/app/layout.test.tsx
+++ apps/frontend/__tests__/unit/app/layout.test.tsx
@@ -1,6 +1,7 @@
 import { describe, vi } from 'vitest';
 
-import RootLayout, { generateMetadata } from '@app/layout.tsx';
+import { FONT_MOCK } from '@app/constants';
+import RootLayout, { generateMetadata } from '@app/layout';
 
 import { injectMatchMediaMock, injectMocks, testNextPage } from '@test/assets';
 
@@ -9,29 +10,11 @@ describe('Root layout test', () => {
   injectMatchMediaMock();
 
   injectMocks(() => {
-    vi.mock(import('next/font/google'), async importOriginal => {
-      const mod = await importOriginal();
-      return {
-        ...mod,
-        // eslint-disable-next-line jsdoc/require-jsdoc
-        Sofia_Sans: () => ({
-          className: '<MOCKED>',
-          style: {
-            fontFamily: '<MOCKED_FONT_FAMILY>',
-          },
-          variable: '<MOCKED_VAR_NAME>',
-        }),
-
-        // eslint-disable-next-line jsdoc/require-jsdoc
-        Playwrite_DE_LA: () => ({
-          className: '<MOCKED>',
-          style: {
-            fontFamily: '<MOCKED_FONT_FAMILY>',
-          },
-          variable: '<MOCKED_VAR_NAME>',
-        }),
-      };
-    });
+    vi.mock('next/font/google', () => ({
+      Inter: FONT_MOCK,
+      Playwrite_DE_LA: FONT_MOCK,
+      Sofia_Sans: FONT_MOCK,
+    }));
   });
 
   testNextPage(<RootLayout children={undefined} />, {
