000751d84d46cc31172bbfd0c1e4140aa9abf5f7
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-04-15 16:04:26 +0200

    ☝️ change mocks in Logo.test

--- apps/frontend/__tests__/unit/components/ui-kit/logo/component.test.tsx
+++ apps/frontend/__tests__/unit/components/ui-kit/logo/component.test.tsx
@@ -2,24 +2,17 @@ import { describe, test, vi } from 'vitest';
 
 import { Logo } from '@/components/ui/kit';
 
+import { FONT_MOCK } from '@app/constants';
+
 import { assertRendering, injectMocks } from '@test/assets';
 
 describe('Logo component', () => {
   injectMocks(() => {
-    vi.mock(import('next/font/google'), async importOriginal => {
-      const mod = await importOriginal();
-      return {
-        ...mod,
-        // eslint-disable-next-line jsdoc/require-jsdoc
-        Playwrite_DE_LA: () => ({
-          className: '<MOCKED>',
-          style: {
-            fontFamily: '<MOCKED_FONT_FAMILY>',
-          },
-          variable: '<MOCKED_VAR_NAME>',
-        }),
-      };
-    });
+    vi.mock('next/font/google', () => ({
+      Inter: FONT_MOCK,
+      Playwrite_DE_LA: FONT_MOCK,
+      Sofia_Sans: FONT_MOCK,
+    }));
   });
 
   test('It renders', () => {
