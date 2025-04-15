26168dd7c9bdc0ecf7cd8682156e6d7d4eac1174
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-04-15 16:11:40 +0200

    🧪 change mocks

--- apps/frontend/__tests__/unit/components/ui-kit/logo/component.test.tsx
+++ apps/frontend/__tests__/unit/components/ui-kit/logo/component.test.tsx
@@ -2,17 +2,13 @@ import { describe, test, vi } from 'vitest';
 
 import { Logo } from '@/components/ui/kit';
 
-import { FONT_MOCK } from '@app/constants';
+import { FONT_MOCKS_OBJECT } from '@app/constants';
 
 import { assertRendering, injectMocks } from '@test/assets';
 
 describe('Logo component', () => {
   injectMocks(() => {
-    vi.mock('next/font/google', () => ({
-      Inter: FONT_MOCK,
-      Playwrite_DE_LA: FONT_MOCK,
-      Sofia_Sans: FONT_MOCK,
-    }));
+    vi.mock('next/font/google', () => FONT_MOCKS_OBJECT);
   });
 
   test('It renders', () => {
--- apps/frontend/app/constants/mock-constants.ts
+++ apps/frontend/app/constants/mock-constants.ts
@@ -1,4 +1,5 @@
 import type { NextFontWithVariable } from 'next/dist/compiled/@next/font';
+import type * as GOOGLE_FONTS from 'next/font/google';
 import type { Fn } from 'xenopomp-essentials';
 
 /** Default mock for any font export. */
@@ -9,3 +10,16 @@ export const FONT_MOCK: Fn<[], NextFontWithVariable> = () => ({
   },
   variable: '--mocked-font-name',
 });
+
+/**
+ * This object contains mocked google fonts from 'next/font/google'.
+ * @example
+ * injectMocks(() => {
+ *   vi.mock('next/font/google', () => FONT_MOCKS_OBJECT);
+ * });
+ */
+export const FONT_MOCKS_OBJECT = {
+  Inter: FONT_MOCK,
+  Sofia_Sans: FONT_MOCK,
+  Playwrite_DE_LA: FONT_MOCK,
+} satisfies Partial<Record<keyof typeof GOOGLE_FONTS, typeof FONT_MOCK>>;
