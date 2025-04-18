0303511081a861766864498ee5d3e69834bc64d0
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-04-18 18:48:45 +0200

    ✍️ create migration tool

--- packages/cli/src/commands/migrate.ts
+++ packages/cli/src/commands/migrate.ts
@@ -1,21 +1,39 @@
 import { Args, Command, Flags } from '@oclif/core';
+import { exec } from 'node:child_process';
+
+interface DefaultOptions {
+  name: string | undefined;
+}
 
 export default class Migrate extends Command {
   static override args = {
-    file: Args.string({ description: 'file to read' }),
+    type: Args.string({
+      name: 'migration type',
+      options: ['prisma'],
+      required: true,
+    }),
   };
-  static override description = 'describe the command here';
-  static override examples = ['<%= config.bin %> <%= command.id %>'];
+  static override description = 'run any type of migrations inside app';
+  static override examples = [
+    '<%= config.bin %> <%= command.id %> prisma - run Prisma migration process with types generation',
+  ];
   static override flags = {
-    // flag with no value (-f, --force)
-    force: Flags.boolean({ char: 'f' }),
     // flag with a value (-n, --name=VALUE)
-    name: Flags.string({ char: 'n', description: 'name to print' }),
+    name: Flags.string({ char: 'n', description: 'name of migration' }),
   };
 
   public async run(): Promise<void> {
-    // const { args, flags } = await this.parse(Migrate);
+    const { args, flags } = await this.parse(Migrate);
+    const { type } = args;
 
-    this.log(`Migrataion from`);
+    switch (type) {
+      case 'prisma': {
+        await this.migratePrisma(flags);
+      }
+    }
+  }
+
+  private async migratePrisma({ name }: DefaultOptions) {
+    exec(`yarn migrate:dev --name ${name} && yarn pack:either`);
   }
 }
